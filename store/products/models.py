5539afe3925f34128bd1ae625c6c68a30354bf78
Azat725 <152897735+Azat725@users.noreply.github.com>
2024-08-12 18:49:52 +0300

    i forgot, what i'm changed

--- store/products/models.py
+++ store/products/models.py
@@ -1,3 +1,15 @@
+from enum import unique
 from django.db import models
 
-# Create your models here.
+class ProductCategory(models.Model):
+	name = models.CharField(max_length=250, unique=True)
+	description = models.TextField(unique=True, blank=True)
+	
+	
+class Product(models.Model):
+	name = models.CharField(max_length=250, unique=True)
+	description = models.TextField(unique=True, blank=True)
+	price = models.DecimalField(max_digits=9999, decimal_places=9)
+	quantity = models.PositiveIntegerField()
+	image = models.ImageField(upload_to="products_images")
+	category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
