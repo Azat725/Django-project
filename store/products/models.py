73bdbd06059272d2584f2e41edc9e6f25e96d664
Azat725 <152897735+Azat725@users.noreply.github.com>
2024-08-14 18:46:02 +0300

    add admin and database

--- store/products/models.py
+++ store/products/models.py
@@ -1,10 +1,14 @@
 from enum import unique
+from typing import Any, SupportsIndex
 from django.db import models
 
 class ProductCategory(models.Model):
 	name = models.CharField(max_length=250, unique=True)
 	description = models.TextField(unique=True, blank=True)
 	
+	
+	def __str__(self) -> str:
+		return self.name
 	
 class Product(models.Model):
 	name = models.CharField(max_length=250, unique=True)
@@ -13,3 +17,4 @@ class Product(models.Model):
 	quantity = models.PositiveIntegerField()
 	image = models.ImageField(upload_to="products_images")
 	category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
+ 
--- store/store/settings.py
+++ store/store/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'products',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
