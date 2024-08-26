0fa57ca44075e461f80dbbc7df910f41c417ee17
Azat725 <152897735+Azat725@users.noreply.github.com>
2024-08-26 19:04:35 +0300

    add users app

--- store/store/urls.py
+++ store/store/urls.py
@@ -5,11 +5,13 @@ from django.conf import settings
 
 from products.views import index
 
+#Набор уникальных путей к именам файлов для перехода на отдельные страницы
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
-    path('products/', include("products.urls", namespace="products")),
+    path('products/', include('products.urls', namespace='products')),
+    path('users/', include('users.urls', namespace='users')),
 ]
 
 if settings.DEBUG == True:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
