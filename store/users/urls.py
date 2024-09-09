6dcec588b02c534761b1522942df655fdf822f24
Azat725 <152897735+Azat725@users.noreply.github.com>
2024-09-09 19:03:24 +0300

    add logout

--- store/users/urls.py
+++ store/users/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
 
-from users.views import login, register, profile
-
+from users.views import login, register, profile, logout
 app_name = 'users'
 
-#Набор уникальных путей к именам файлов для перехода на отдельные страницы
 urlpatterns = [
     path('login/', login, name='login'),
     path('register/', register, name='register'),
-    path('profile/', profile, name='profile')
+    path('profile/', profile, name='profile'),
+    path('logout/', logout, name="logout"),
 ]
--- store/users/views.py
+++ store/users/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, HttpResponseRedirect
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
-from django.contrib import auth
+from django.contrib import auth, messages
 from django.urls import reverse
 from users.models import User
+
 
 def login(request):
     if request.method == 'POST':
@@ -14,34 +15,55 @@ def login(request):
             if user:
                 auth.login(request, user)
                 return HttpResponseRedirect(reverse('index'))
+            else:
+                context = {'form': form, 'error': 'Invalid login or password'}
+                return render(request, 'users/login.html', context)
     else:
         form = UserLoginForm()
     
     context = {
-        'form': form,
+        'form': form
     }
+    
     return render(request, 'users/login.html', context)
+
 
 def register(request):
     if request.method == 'POST':
         form = UserRegistrationForm(data=request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, "Вы успешно зарегестрировались")
             return HttpResponseRedirect(reverse('users:login'))
     else:
         form = UserRegistrationForm()
     
-    context = {'form': form}
+    context = {
+        'form': form
+    }
+    
     return render(request, 'users/register.html', context)
 
 
-
 def profile(request):
-    if request.method == "POST":
-        form = UserProfileForm(isinstance=request.user, data=request.POST, files=request.FILES)
+    if request.method == 'POST':
+        form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
         if form.is_valid():
             form.save()
-            return 
+            return HttpResponseRedirect(reverse('users:profile'))
         else:
-            return form.add_error()
-    return render(request, 'users/profile.html')
+            print(form.errors)
+    else:
+        form = UserProfileForm(instance=request.user)
+    
+    context = {
+        'title': 'Store - профиль',
+        'form': form
+    }
+    
+    return render(request, 'users/profile.html', context)
+
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse("index")) 
